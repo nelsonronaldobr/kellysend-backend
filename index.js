226872e6ca79bdbe8e2d4842f8cc07b4ac50dad0
NelsonBarreraRivas <nelsonbr862@gmail.com>
2023-05-19 23:28:06 -0500

    return cors

--- index.js
+++ index.js
@@ -14,24 +14,9 @@ connectDB();
 // Lectura y parseo del body
 app.use(express.json());
 
+app.use(cors())
+
 app.use(express.static('uploads'))
-
-const whiteList = [
-    process.env.FRONTEND_URL_BASE
-];
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (whiteList.includes(origin)) {
-            callback(null, true)
-
-        } else {
-            callback(new Error("Error de Cors"))
-        }
-    },
-}
-
-app.use(cors(corsOptions))
 
 
 /* -------------------------------------------------------------------------- */
